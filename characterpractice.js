ffa614cd3f9f1e3dd76dd0cdf6b0399c89b65539
Luke Johnson <ljohn078@ucr.edu>
2025-07-27 19:35:42 -0700

    removed unecisary 'meaning' from character data

--- characterpractice.js
+++ characterpractice.js
@@ -1,96 +1,94 @@
 function CharacterPractice () {
     const hiraganaCharacters = {
         //  a            i           u            e            o
-        'あ': { romaji: 'a', meaning: '' },   'い': { romaji: 'i', meaning: '' },   'う': { romaji: 'u', meaning: '' },   'え': { romaji: 'e', meaning: '' },   'お': { romaji: 'o', meaning: '' },
+        'あ': { romaji: 'a' },   'い': { romaji: 'i' },   'う': { romaji: 'u' },   'え': { romaji: 'e' },   'お': { romaji: 'o' },
         //  k-series
-        'か': { romaji: 'ka', meaning: '' },  'き': { romaji: 'ki', meaning: '' },  'く': { romaji: 'ku', meaning: '' },  'け': { romaji: 'ke', meaning: '' },  'こ': { romaji: 'ko', meaning: '' },
-        'が': { romaji: 'ga', meaning: '' },  'ぎ': { romaji: 'gi', meaning: '' },  'ぐ': { romaji: 'gu', meaning: '' },  'げ': { romaji: 'ge', meaning: '' },  'ご': { romaji: 'go', meaning: '' },
+        'か': { romaji: 'ka' },  'き': { romaji: 'ki' },  'く': { romaji: 'ku' },  'け': { romaji: 'ke' },  'こ': { romaji: 'ko' },
+        'が': { romaji: 'ga' },  'ぎ': { romaji: 'gi' },  'ぐ': { romaji: 'gu' },  'げ': { romaji: 'ge' },  'ご': { romaji: 'go' },
         //  s-series
-        'さ': { romaji: 'sa', meaning: '' },  'し': { romaji: 'shi', meaning: '' }, 'す': { romaji: 'su', meaning: '' },  'せ': { romaji: 'se', meaning: '' },  'そ': { romaji: 'so', meaning: '' },
-        'ざ': { romaji: 'za', meaning: '' },  'じ': { romaji: 'ji', meaning: '' },  'ず': { romaji: 'zu', meaning: '' },  'ぜ': { romaji: 'ze', meaning: '' },  'ぞ': { romaji: 'zo', meaning: '' },
+        'さ': { romaji: 'sa' },  'し': { romaji: 'shi' }, 'す': { romaji: 'su' },  'せ': { romaji: 'se' },  'そ': { romaji: 'so' },
+        'ざ': { romaji: 'za' },  'じ': { romaji: 'ji' },  'ず': { romaji: 'zu' },  'ぜ': { romaji: 'ze' },  'ぞ': { romaji: 'zo' },
         //  t-series
-        'た': { romaji: 'ta', meaning: '' },  'ち': { romaji: 'chi', meaning: '' }, 'つ': { romaji: 'tsu', meaning: '' }, 'て': { romaji: 'te', meaning: '' },  'と': { romaji: 'to', meaning: '' },
-        'だ': { romaji: 'da', meaning: '' },  'ぢ': { romaji: 'ji', meaning: '' },  'づ': { romaji: 'zu', meaning: '' },  'で': { romaji: 'de', meaning: '' },  'ど': { romaji: 'do', meaning: '' },
+        'た': { romaji: 'ta' },  'ち': { romaji: 'chi' }, 'つ': { romaji: 'tsu' }, 'て': { romaji: 'te' },  'と': { romaji: 'to' },
+        'だ': { romaji: 'da' },  'ぢ': { romaji: 'ji' },  'づ': { romaji: 'zu' },  'で': { romaji: 'de' },  'ど': { romaji: 'do' },
         //  n-series
-        'な': { romaji: 'na', meaning: '' },  'に': { romaji: 'ni', meaning: '' },  'ぬ': { romaji: 'nu', meaning: '' },  'ね': { romaji: 'ne', meaning: '' },  'の': { romaji: 'no', meaning: '' },
+        'な': { romaji: 'na' },  'に': { romaji: 'ni' },  'ぬ': { romaji: 'nu' },  'ね': { romaji: 'ne' },  'の': { romaji: 'no' },
         //  h-series
-        'は': { romaji: 'ha', meaning: '' },  'ひ': { romaji: 'hi', meaning: '' },  'ふ': { romaji: 'fu', meaning: '' },  'へ': { romaji: 'he', meaning: '' },  'ほ': { romaji: 'ho', meaning: '' },
-        'ば': { romaji: 'ba', meaning: '' },  'び': { romaji: 'bi', meaning: '' },  'ぶ': { romaji: 'bu', meaning: '' },  'べ': { romaji: 'be', meaning: '' },  'ぼ': { romaji: 'bo', meaning: '' },
-        'ぱ': { romaji: 'pa', meaning: '' },  'ぴ': { romaji: 'pi', meaning: '' },  'ぷ': { romaji: 'pu', meaning: '' },  'ぺ': { romaji: 'pe', meaning: '' },  'ぽ': { romaji: 'po', meaning: '' },
+        'は': { romaji: 'ha' },  'ひ': { romaji: 'hi' },  'ふ': { romaji: 'fu' },  'へ': { romaji: 'he' },  'ほ': { romaji: 'ho' },
+        'ば': { romaji: 'ba' },  'び': { romaji: 'bi' },  'ぶ': { romaji: 'bu' },  'べ': { romaji: 'be' },  'ぼ': { romaji: 'bo' },
+        'ぱ': { romaji: 'pa' },  'ぴ': { romaji: 'pi' },  'ぷ': { romaji: 'pu' },  'ぺ': { romaji: 'pe' },  'ぽ': { romaji: 'po' },
         //  m-series
-        'ま': { romaji: 'ma', meaning: '' },  'み': { romaji: 'mi', meaning: '' },  'む': { romaji: 'mu', meaning: '' },  'め': { romaji: 'me', meaning: '' },  'も': { romaji: 'mo', meaning: '' },
+        'ま': { romaji: 'ma' },  'み': { romaji: 'mi' },  'む': { romaji: 'mu' },  'め': { romaji: 'me' },  'も': { romaji: 'mo' },
         //  y-series
-        'や': { romaji: 'ya', meaning: '' },                                         'ゆ': { romaji: 'yu', meaning: '' },                                         'よ': { romaji: 'yo', meaning: '' },
+        'や': { romaji: 'ya' },                           'ゆ': { romaji: 'yu' },                           'よ': { romaji: 'yo' },
         //  r-series
-        'ら': { romaji: 'ra', meaning: '' },  'り': { romaji: 'ri', meaning: '' },  'る': { romaji: 'ru', meaning: '' },  'れ': { romaji: 're', meaning: '' },  'ろ': { romaji: 'ro', meaning: '' },
-        'わ': { romaji: 'wa', meaning: '' },                                                                                                                   'を': { romaji: 'wo', meaning: '' },
-        'ん': { romaji: 'n', meaning: '' },
+        'ら': { romaji: 'ra' },  'り': { romaji: 'ri' },  'る': { romaji: 'ru' },  'れ': { romaji: 're' },  'ろ': { romaji: 'ro' },
+        'わ': { romaji: 'wa' },                                                                             'を': { romaji: 'wo' },
+        'ん': { romaji: 'n' },
 
         // Yoon (contracted sounds)
-        'きゃ': { romaji: 'kya', meaning: '' },          'きゅ': { romaji: 'kyu', meaning: '' },             'きょ': { romaji: 'kyo', meaning: '' },
-        'しゃ': { romaji: 'sha', meaning: '' },          'しゅ': { romaji: 'shu', meaning: '' },             'しょ': { romaji: 'sho', meaning: '' },
-        'ちゃ': { romaji: 'cha', meaning: '' },          'ちゅ': { romaji: 'chu', meaning: '' },             'ちょ': { romaji: 'cho', meaning: '' },
-        'にゃ': { romaji: 'nya', meaning: '' },          'にゅ': { romaji: 'nyu', meaning: '' },             'にょ': { romaji: 'nyo', meaning: '' },
-        'ひゃ': { romaji: 'hya', meaning: '' },          'ひゅ': { romaji: 'hyu', meaning: '' },             'ひょ': { romaji: 'hyo', meaning: '' },
-        'みゃ': { romaji: 'mya', meaning: '' },          'みゅ': { romaji: 'myu', meaning: '' },             'みょ': { romaji: 'myo', meaning: '' },
-        'りゃ': { romaji: 'rya', meaning: '' },          'りゅ': { romaji: 'ryu', meaning: '' },             'りょ': { romaji: 'ryo', meaning: '' },
-        'ぎゃ': { romaji: 'gya', meaning: '' },          'ぎゅ': { romaji: 'gyu', meaning: '' },             'ぎょ': { romaji: 'gyo', meaning: '' },
-        'じゃ': { romaji: 'ja', meaning: '' },           'じゅ': { romaji: 'ju', meaning: '' },              'じょ': { romaji: 'jo', meaning: '' },
-        'びゃ': { romaji: 'bya', meaning: '' },          'びゅ': { romaji: 'byu', meaning: '' },             'びょ': { romaji: 'byo', meaning: '' },
-        'ぴゃ': { romaji: 'pya', meaning: '' },          'ぴゅ': { romaji: 'pyu', meaning: '' },             'ぴょ': { romaji: 'pyo', meaning: '' }
+        'きゃ': { romaji: 'kya' },          'きゅ': { romaji: 'kyu' },             'きょ': { romaji: 'kyo' },
+        'しゃ': { romaji: 'sha' },          'しゅ': { romaji: 'shu' },             'しょ': { romaji: 'sho' },
+        'ちゃ': { romaji: 'cha' },          'ちゅ': { romaji: 'chu' },             'ちょ': { romaji: 'cho' },
+        'にゃ': { romaji: 'nya' },          'にゅ': { romaji: 'nyu' },             'にょ': { romaji: 'nyo' },
+        'ひゃ': { romaji: 'hya' },          'ひゅ': { romaji: 'hyu' },             'ひょ': { romaji: 'hyo' },
+        'みゃ': { romaji: 'mya' },          'みゅ': { romaji: 'myu' },             'みょ': { romaji: 'myo' },
+        'りゃ': { romaji: 'rya' },          'りゅ': { romaji: 'ryu' },             'りょ': { romaji: 'ryo' },
+        'ぎゃ': { romaji: 'gya' },          'ぎゅ': { romaji: 'gyu' },             'ぎょ': { romaji: 'gyo' },
+        'じゃ': { romaji: 'ja' },           'じゅ': { romaji: 'ju' },              'じょ': { romaji: 'jo' },
+        'びゃ': { romaji: 'bya' },          'びゅ': { romaji: 'byu' },             'びょ': { romaji: 'byo' },
+        'ぴゃ': { romaji: 'pya' },          'ぴゅ': { romaji: 'pyu' },             'ぴょ': { romaji: 'pyo' }
     };
 
     const katakanaCharacters = {
         //  a            i           u            e            o
-        'ア': { romaji: 'a', meaning: '' },   'イ': { romaji: 'i', meaning: '' },   'ウ': { romaji: 'u', meaning: '' },   'エ': { romaji: 'e', meaning: '' },   'オ': { romaji: 'o', meaning: '' },
+        'ア': { romaji: 'a' },   'イ': { romaji: 'i' },   'ウ': { romaji: 'u' },   'エ': { romaji: 'e' },   'オ': { romaji: 'o' },
         //  k-series
-        'カ': { romaji: 'ka', meaning: '' },  'キ': { romaji: 'ki', meaning: '' },  'ク': { romaji: 'ku', meaning: '' },  'ケ': { romaji: 'ke', meaning: '' },  'コ': { romaji: 'ko', meaning: '' },
-        'ガ': { romaji: 'ga', meaning: '' },  'ギ': { romaji: 'gi', meaning: '' },  'グ': { romaji: 'gu', meaning: '' },  'ゲ': { romaji: 'ge', meaning: '' },  'ゴ': { romaji: 'go', meaning: '' },
+        'カ': { romaji: 'ka' },  'キ': { romaji: 'ki' },  'ク': { romaji: 'ku' },  'ケ': { romaji: 'ke' },  'コ': { romaji: 'ko' },
+        'ガ': { romaji: 'ga' },  'ギ': { romaji: 'gi' },  'グ': { romaji: 'gu' },  'ゲ': { romaji: 'ge' },  'ゴ': { romaji: 'go' },
         //  s-series
-        'サ': { romaji: 'sa', meaning: '' },  'シ': { romaji: 'shi', meaning: '' }, 'ス': { romaji: 'su', meaning: '' },  'セ': { romaji: 'se', meaning: '' },  'ソ': { romaji: 'so', meaning: '' },
-        'ザ': { romaji: 'za', meaning: '' },  'ジ': { romaji: 'ji', meaning: '' },  'ズ': { romaji: 'zu', meaning: '' },  'ゼ': { romaji: 'ze', meaning: '' },  'ゾ': { romaji: 'zo', meaning: '' },
+        'サ': { romaji: 'sa' },  'シ': { romaji: 'shi' }, 'ス': { romaji: 'su' },  'セ': { romaji: 'se' },  'ソ': { romaji: 'so' },
+        'ザ': { romaji: 'za' },  'ジ': { romaji: 'ji' },  'ズ': { romaji: 'zu' },  'ゼ': { romaji: 'ze' },  'ゾ': { romaji: 'zo' },
         //  t-series
-        'タ': { romaji: 'ta', meaning: '' },  'チ': { romaji: 'chi', meaning: '' }, 'ツ': { romaji: 'tsu', meaning: '' }, 'テ': { romaji: 'te', meaning: '' },  'ト': { romaji: 'to', meaning: '' },
-        'ダ': { romaji: 'da', meaning: '' },  'ヂ': { romaji: 'ji', meaning: '' },  'ヅ': { romaji: 'zu', meaning: '' },  'デ': { romaji: 'de', meaning: '' },  'ド': { romaji: 'do', meaning: '' },
+        'タ': { romaji: 'ta' },  'チ': { romaji: 'chi' }, 'ツ': { romaji: 'tsu' }, 'テ': { romaji: 'te' },  'ト': { romaji: 'to' },
+        'ダ': { romaji: 'da' },  'ヂ': { romaji: 'ji' },  'ヅ': { romaji: 'zu' },  'デ': { romaji: 'de' },  'ド': { romaji: 'do' },
         //  n-series
-        'ナ': { romaji: 'na', meaning: '' },  'ニ': { romaji: 'ni', meaning: '' },  'ヌ': { romaji: 'nu', meaning: '' },  'ネ': { romaji: 'ne', meaning: '' },  'ノ': { romaji: 'no', meaning: '' },
+        'ナ': { romaji: 'na' },  'ニ': { romaji: 'ni' },  'ヌ': { romaji: 'nu' },  'ネ': { romaji: 'ne' },  'ノ': { romaji: 'no' },
         //  h-series
-        'ハ': { romaji: 'ha', meaning: '' },  'ヒ': { romaji: 'hi', meaning: '' },  'フ': { romaji: 'fu', meaning: '' },  'ヘ': { romaji: 'he', meaning: '' },  'ホ': { romaji: 'ho', meaning: '' },
-        'バ': { romaji: 'ba', meaning: '' },  'ビ': { romaji: 'bi', meaning: '' },  'ブ': { romaji: 'bu', meaning: '' },  'ベ': { romaji: 'be', meaning: '' },  'ボ': { romaji: 'bo', meaning: '' },
-        'パ': { romaji: 'pa', meaning: '' },  'ピ': { romaji: 'pi', meaning: '' },  'プ': { romaji: 'pu', meaning: '' },  'ペ': { romaji: 'pe', meaning: '' },  'ポ': { romaji: 'po', meaning: '' },
+        'ハ': { romaji: 'ha' },  'ヒ': { romaji: 'hi' },  'フ': { romaji: 'fu' },  'ヘ': { romaji: 'he' },  'ホ': { romaji: 'ho' },
+        'バ': { romaji: 'ba' },  'ビ': { romaji: 'bi' },  'ブ': { romaji: 'bu' },  'ベ': { romaji: 'be' },  'ボ': { romaji: 'bo' },
+        'パ': { romaji: 'pa' },  'ピ': { romaji: 'pi' },  'プ': { romaji: 'pu' },  'ペ': { romaji: 'pe' },  'ポ': { romaji: 'po' },
         //  m-series
-        'マ': { romaji: 'ma', meaning: '' },  'ミ': { romaji: 'mi', meaning: '' },  'ム': { romaji: 'mu', meaning: '' },  'メ': { romaji: 'me', meaning: '' },  'モ': { romaji: 'mo', meaning: '' },
+        'マ': { romaji: 'ma' },  'ミ': { romaji: 'mi' },  'ム': { romaji: 'mu' },  'メ': { romaji: 'me' },  'モ': { romaji: 'mo' },
         //  y-series
-        'ヤ': { romaji: 'ya', meaning: '' },                                         'ユ': { romaji: 'yu', meaning: '' },                                         'ヨ': { romaji: 'yo', meaning: '' },
+        'ヤ': { romaji: 'ya' },                           'ユ': { romaji: 'yu' },                           'ヨ': { romaji: 'yo' },
         //  r-series
-        'ラ': { romaji: 'ra', meaning: '' },  'リ': { romaji: 'ri', meaning: '' },  'ル': { romaji: 'ru', meaning: '' },  'レ': { romaji: 're', meaning: '' },  'ロ': { romaji: 'ro', meaning: '' },
-        'ワ': { romaji: 'wa', meaning: '' },  'ヰ': { romaji: 'wi', meaning: '' },  'ヱ': { romaji: 'we', meaning: '' },  'ヲ': { romaji: 'wo', meaning: '' },
-        'ン': { romaji: 'n', meaning: '' },
+        'ラ': { romaji: 'ra' },  'リ': { romaji: 'ri' },  'ル': { romaji: 'ru' },  'レ': { romaji: 're' },  'ロ': { romaji: 'ro' },
+        'ワ': { romaji: 'wa' },  'ヰ': { romaji: 'wi' },  'ヱ': { romaji: 'we' },  'ヲ': { romaji: 'wo' },
+        'ン': { romaji: 'n' },
 
         // Yoon (contracted sounds)
-        'キャ': { romaji: 'kya', meaning: '' },          'キュ': { romaji: 'kyu', meaning: '' },             'キョ': { romaji: 'kyo', meaning: '' },
-        'シャ': { romaji: 'sha', meaning: '' },          'シュ': { romaji: 'shu', meaning: '' },             'ショ': { romaji: 'sho', meaning: '' },
-        'チャ': { romaji: 'cha', meaning: '' },          'チュ': { romaji: 'chu', meaning: '' },             'チョ': { romaji: 'cho', meaning: '' },
-        'ニャ': { romaji: 'nya', meaning: '' },          'ニュ': { romaji: 'nyu', meaning: '' },             'ニョ': { romaji: 'nyo', meaning: '' },
-        'ヒャ': { romaji: 'hya', meaning: '' },          'ヒュ': { romaji: 'hyu', meaning: '' },             'ヒョ': { romaji: 'hyo', meaning: '' },
-        'ミャ': { romaji: 'mya', meaning: '' },          'ミュ': { romaji: 'myu', meaning: '' },             'ミョ': { romaji: 'myo', meaning: '' },
-        'リャ': { romaji: 'rya', meaning: '' },          'リュ': { romaji: 'ryu', meaning: '' },             'リョ': { romaji: 'ryo', meaning: '' },
-        'ギャ': { romaji: 'gya', meaning: '' },          'ギュ': { romaji: 'gyu', meaning: '' },             'ギョ': { romaji: 'gyo', meaning: '' },
-        'ジャ': { romaji: 'ja', meaning: '' },           'ジュ': { romaji: 'ju', meaning: '' },              'ジョ': { romaji: 'jo', meaning: '' },
-        'ビャ': { romaji: 'bya', meaning: '' },          'ビュ': { romaji: 'byu', meaning: '' },             'ビョ': { romaji: 'byo', meaning: '' },
-        'ピャ': { romaji: 'pya', meaning: '' },          'ピュ': { romaji: 'pyu', meaning: '' },             'ピョ': { romaji: 'pyo', meaning: '' },
+        'キャ': { romaji: 'kya' },          'キュ': { romaji: 'kyu' },             'キョ': { romaji: 'kyo' },
+        'シャ': { romaji: 'sha' },          'シュ': { romaji: 'shu' },             'ショ': { romaji: 'sho' },
+        'チャ': { romaji: 'cha' },          'チュ': { romaji: 'chu' },             'チョ': { romaji: 'cho' },
+        'ニャ': { romaji: 'nya' },          'ニュ': { romaji: 'nyu' },             'ニョ': { romaji: 'nyo' },
+        'ヒャ': { romaji: 'hya' },          'ヒュ': { romaji: 'hyu' },             'ヒョ': { romaji: 'hyo' },
+        'ミャ': { romaji: 'mya' },          'ミュ': { romaji: 'myu' },             'ミョ': { romaji: 'myo' },
+        'リャ': { romaji: 'rya' },          'リュ': { romaji: 'ryu' },             'リョ': { romaji: 'ryo' },
+        'ギャ': { romaji: 'gya' },          'ギュ': { romaji: 'gyu' },             'ギョ': { romaji: 'gyo' },
+        'ジャ': { romaji: 'ja' },           'ジュ': { romaji: 'ju' },              'ジョ': { romaji: 'jo' },
+        'ビャ': { romaji: 'bya' },          'ビュ': { romaji: 'byu' },             'ビョ': { romaji: 'byo' },
+        'ピャ': { romaji: 'pya' },          'ピュ': { romaji: 'pyu' },             'ピョ': { romaji: 'pyo' },
         
         // Extended katakana for foreign words
-        'ヴ': { romaji: 'vu', meaning: '' },
-        'ティ': { romaji: 'ti', meaning: '' },           'ディ': { romaji: 'di', meaning: '' },              'トゥ': { romaji: 'tu', meaning: '' },
-        'ドゥ': { romaji: 'du', meaning: '' },           'ファ': { romaji: 'fa', meaning: '' },              'フィ': { romaji: 'fi', meaning: '' },
-        'フェ': { romaji: 'fe', meaning: '' },           'フォ': { romaji: 'fo', meaning: '' },              'ウィ': { romaji: 'wi', meaning: '' },
-        'ウェ': { romaji: 'we', meaning: '' },           'ウォ': { romaji: 'wo', meaning: '' },              'ツァ': { romaji: 'tsa', meaning: '' },
-        'ツィ': { romaji: 'tsi', meaning: '' },          'ツェ': { romaji: 'tse', meaning: '' },             'ツォ': { romaji: 'tso', meaning: '' },
-        'チェ': { romaji: 'che', meaning: '' },          'シェ': { romaji: 'she', meaning: '' },             'ジェ': { romaji: 'je', meaning: '' },
-        'イェ': { romaji: 'ye', meaning: '' }
+        'ヴ': { romaji: 'vu' },
+        'ティ': { romaji: 'ti' },           'ディ': { romaji: 'di' },              'トゥ': { romaji: 'tu' },
+        'ドゥ': { romaji: 'du' },           'ファ': { romaji: 'fa' },              'フィ': { romaji: 'fi' },
+        'フェ': { romaji: 'fe' },           'フォ': { romaji: 'fo' },              'ウィ': { romaji: 'wi' },
+        'ウェ': { romaji: 'we' },           'ウォ': { romaji: 'wo' },              'ツァ': { romaji: 'tsa' },
+        'ツィ': { romaji: 'tsi' },          'ツェ': { romaji: 'tse' },             'ツォ': { romaji: 'tso' },
+        'チェ': { romaji: 'che' },          'シェ': { romaji: 'she' },             'ジェ': { romaji: 'je' },
+        'イェ': { romaji: 'ye' }
     };
-
-    
 
 }
